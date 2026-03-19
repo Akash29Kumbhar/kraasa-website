ac159ee03bec993717f87cdf98fcc9f5f3ea613d
Akash29Kumbhar <Akash.2kumbhar@gmail.com>
2026-03-19 11:22:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import KraasaWebsite from "./App";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<KraasaWebsite />);
